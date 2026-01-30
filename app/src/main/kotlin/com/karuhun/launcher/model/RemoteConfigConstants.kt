d244dd17bc001730b632f5a762e740ce30807898
headhonco7 <headhonco7@gmail.com>
2026-01-30 15:59:43 +0700

    Update RemoteConfigConstants.kt

--- app/src/main/kotlin/com/karuhun/launcher/model/RemoteConfigConstants.kt
+++ app/src/main/kotlin/com/karuhun/launcher/model/RemoteConfigConstants.kt
@@ -1,4 +1,4 @@
-package com.karuhun.launcher
+package com.karuhun.launcher.model
 
 object RemoteConfigConstants {
     const val CONFIG_URL = "https://headhonco7.github.io/AzkaLauncherLite/config/config.json"
