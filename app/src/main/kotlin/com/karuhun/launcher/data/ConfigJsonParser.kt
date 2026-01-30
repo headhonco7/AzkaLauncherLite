573d489e6c3cb67bc70db2d5d7e4b69df192db11
headhonco7 <headhonco7@gmail.com>
2026-01-31 00:39:34 +0700

    Update ConfigJsonParser.kt

--- app/src/main/kotlin/com/karuhun/launcher/data/ConfigJsonParser.kt
+++ app/src/main/kotlin/com/karuhun/launcher/data/ConfigJsonParser.kt
@@ -6,18 +6,23 @@ import com.karuhun.launcher.model.Ui
 import com.karuhun.launcher.model.Whatsapp
 import com.karuhun.launcher.model.Wifi
 import org.json.JSONObject
+import com.karuhun.launcher.model.Branding
 
 object ConfigJsonParser {
 
     fun parse(raw: String): Config {
         val root = JSONObject(raw)
-
+        val brandingObj = root.optJSONObject("branding") ?: JSONObject()
         val wifiObj = root.optJSONObject("wifi") ?: JSONObject()
         val waObj = root.optJSONObject("whatsapp") ?: JSONObject()
         val textObj = root.optJSONObject("text") ?: JSONObject()
         val uiObj = root.optJSONObject("ui") ?: JSONObject()
 
         return Config(
+            branding = Branding(
+                wallpaperUrl = brandingObj.optString("wallpaperUrl", ""),
+                logoUrl = brandingObj.optString("logoUrl", "")
+            )
             propertyName = root.optString("propertyName", ""),
             city = root.optString("city", ""),
             wifi = Wifi(
