ef5f0a66f262eb477efc24a5935bcb950f0ace80
headhonco7 <headhonco7@gmail.com>
2026-01-30 14:03:56 +0700

    Create Config.kt

---
+++ app/src/main/kotlin/com/karuhun/launcher/model/Config.kt
@@ -0,0 +1,15 @@
+package com.karuhun.launcher
+
+data class Config(
+    val propertyName: String = "",
+    val city: String = "",
+    val wifi: Wifi = Wifi(),
+    val whatsapp: Whatsapp = Whatsapp(),
+    val text: Text = Text(),
+    val ui: Ui = Ui()
+)
+
+data class Wifi(val ssid: String = "", val password: String = "")
+data class Whatsapp(val number: String = "", val label: String = "")
+data class Text(val welcomeHome: String = "", val runningText: String = "")
+data class Ui(val cardZoomEnabled: Boolean = true, val qrEnabled: Boolean = true)
